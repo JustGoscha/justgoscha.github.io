5ffc9923578ac3c5ed0d56c93a1f686dd0d0bcaa
justgoscha <justgoscha@googlemail.com>
2014-04-11 11:54:49 +0200

    updated webaudio experiment

--- experiments/webaudio1/style/script/demo2.js
+++ experiments/webaudio1/style/script/demo2.js
@@ -17,17 +17,12 @@ angular.module('app').directive("listenermove", function(EntityService, SoundSer
 				null
 			);
 
-			var mouse = new EntityService.Entity({x:0,y:0},{x:5,y:5},'#f36');
-
-			EntityService.addMouse(mouse);
-			EntityService.drawingChain.addEntity(mouse);
 			EntityService.addListenerEntity(listener);
 			EntityService.drawingChain.addEntity(listener);
 
 			EntityService.startDrawing();
 
 			var movement = EntityService.movement;
-			var mousePosition = EntityService.mousePosition;
 
 			// controls W S A D
 			angular.element('html').bind('keydown',function(event){
@@ -77,7 +72,7 @@ angular.module('app').directive("listenermove", function(EntityService, SoundSer
 				console.log('Key up:' +event.which);
 				switch (event.which){
 					case 87: {
-						event.preventDefault();
+					event.preventDefault();
 						// up
 						movement.up = false;
 
@@ -116,12 +111,6 @@ angular.module('app').directive("listenermove", function(EntityService, SoundSer
 				}
 			});
 
-			angular.element('html').bind('mousemove',function(event){
-				var rect = element[0].getBoundingClientRect();
-				EntityService.mousePosition.x = event.clientX - rect.left;
-				EntityService.mousePosition.y = event.clientY - rect.top; 
-			});
-
 
 		
 
@@ -148,12 +137,7 @@ angular.module('app').factory('EntityService', function(SoundService){
 
 	var refreshrate = EntityService.refreshrate;
 	var listener = null;
-	var mouse = null;
 
-
-	EntityService.addMouse = function(m){
-		mouse = m;
-	}
 	EntityService.addListenerEntity = function(l){
 		listener = l;
 	}
@@ -218,13 +202,7 @@ angular.module('app').factory('EntityService', function(SoundService){
 		}
 
 		updateMovement();
-		updateMouseMovement();
 	}
-
-	EntityService.mousePosition = {
-		x: 0,
-		y: 0
-	};
 
 	EntityService.movement = {
 		up: false,
@@ -253,27 +231,6 @@ angular.module('app').factory('EntityService', function(SoundService){
 	}
 
 	var lastTime = null;
-
-	function updateMouseMovement () {
-		mouse.position = EntityService.mousePosition;
-		ctx.beginPath();
-		ctx.moveTo(listener.position.x,listener.position.y);
-		ctx.lineTo(mouse.position.x,mouse.position.y);
-		ctx.stroke();
-
-		var u = {
-			x: mouse.position.x - listener.position.x, 
-			y: mouse.position.y - listener.position.y 
-		};
-		var normVal = Math.sqrt(u.x*u.x + u.y*u.y);
-		u.x =  u.x/normVal;
-		u.y = u.y/normVal;
-		// x^2+y^2 = normval * normval ...
-		// x/normVal ... y/normVal
-		// 
-		SoundService.context.listener.setOrientation(u.x,0,u.y,0,1,0);	
-	}
-
 
 	function updateMovement(){
 		var up = movement.up;
@@ -343,10 +300,12 @@ angular.module('app').factory('EntityService', function(SoundService){
 			listener.dimensions.y = Math.pow(listener.dimensions.y,1.01);
 		}
 
-		SoundService.context.listener.setPosition(listener.position.x/50, 1.8, listener.position.y/50)
+		SoundService.context.listener.setPosition(listener.position.x/50, 1.8, -listener.position.y/50)
 		SoundService.context.listener.setVelocity(dx/dt, 0, dy/dt);
 
 		// random effects
+
+
 
 	}
 
@@ -359,9 +318,8 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 
 	var soundSource0 = new EntityService.Entity({x: 50, y: 30}, {x:14,y:14},'#4bf');
 	var soundSource1 = new EntityService.Entity({x: 345, y: 189}, {x:14,y:14},'#4bf');
-	var soundSourceNoise = new EntityService.Entity({x: 420, y: 304}, {x:22,y:22},'#bf4');
 
-	var soundSources = [soundSource0, soundSource1, null,null,null,null,null,null,soundSourceNoise];
+	var soundSources = [soundSource0, soundSource1];
 
 	// EntityService.drawingChain.addEntity(soundSource0);
 	// EntityService.drawingChain.addEntity(soundSource1);
@@ -393,24 +351,9 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 		loadedSounds = bufferList;
 		for(i in bufferList){
 			mixToMono(bufferList[i]);
+			var source = SoundService.context.createBufferSource();
+
 		}
-
-		var node = SoundService.context.createBufferSource()
-		  , buffer = SoundService.context.createBuffer(1, 80000, SoundService.context.sampleRate)
-		  , data = buffer.getChannelData(0);
-
-		for (var i = 0; i < 80000; i++) {
-		 data[i] = Math.random();
-		}
-
-		node.buffer = buffer;
-		//var buffer = SoundService.context.createBuffer(data);
-
-		bufferList.push(buffer);
-		console.log('noise registered as: ' + (bufferList.length-1));
-
-
-
 		$rootScope.$apply();
 	}
 
@@ -418,7 +361,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 
 	var startedMusic = [false, false];
 	var musicSources = [null, null]
-	SoundEntity.startMusicSamples = function(i,loop){
+	SoundEntity.startMusicSamples = function(i){
 		if(startedMusic[i]){
 			startedMusic[i] = false;
 			EntityService.drawingChain.removeEntity(soundSources[i]);
@@ -426,21 +369,20 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 			return;
 		}
 
-		// var convolver0 = SoundService.context.createConvolver();
-		// convolver0.buffer = loadedSounds[2];
+		var convolver0 = SoundService.context.createConvolver();
+		convolver0.buffer = loadedSounds[2];
 
 		var source0 = SoundService.context.createBufferSource();
-		if(loop) source0.loop = true;
 		source0.buffer = loadedSounds[i];
 
 		// TODO maybe the creation of a buffer is critical bottleneck... keep in mind for later
 		musicSources[i] = source0;
 
 		var panner0 = SoundService.context.createPanner();
-		source0.connect(panner0);
-		// convolver0.connect(panner0);
+		source0.connect(convolver0);
+		convolver0.connect(panner0);
 
-		panner0.setPosition(soundSources[i].position.x/50,1,soundSources[i].position.y/50);
+		panner0.setPosition(soundSources[i].position.x/50,1,-soundSources[i].position.y/50);
 		source0.playbackRate.value = 1.0;
 		panner0.connect(SoundService.context.destination);
 
@@ -460,7 +402,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 			var gain0 = SoundService.context.createGain();
 			source0.connect(panner0);
 
-			panner0.setPosition(x/50,1,y/50);
+			panner0.setPosition(x/50,1,-y/50);
 
 			panner0.connect(gain0);
 			gain0.connect(masterGain);
@@ -477,7 +419,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 			var x = Math.random()*600;
 			var y = Math.random()*400;
 			EntityService.drawingChain.addEntity(new EntityService.Entity({x: x, y: y}, {x:14,y:14},'#7cf'));
-			panner0.setPosition(x/50,1,y/50);	
+			panner0.setPosition(x/50,1,-y/50);	
 
 			source0.connect(delay);
 			delay.connect(gain);
@@ -520,7 +462,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 
 			source.connect(panner);
 
-			var x = Math.random()*1000; // Math.random()*60+300;
+			var x =  Math.random()*1000; // Math.random()*60+300;
 			var y = Math.random()*200;
 			var z = Math.random()*1000; // Math.random()*60+30;
 
@@ -530,7 +472,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 				EntityService.drawingChain.removeEntity(effectEntity);
 			},700);
 
-			panner.setPosition(x/50,y/50,z/50);
+			panner.setPosition(x/50,y/50,-z/50);
 			source.playbackRate.value = 1.2-(Math.random()*0.3);
 
 
@@ -590,7 +532,7 @@ angular.module('app').factory('SoundEntity', function($rootScope, SoundService, 
 			},1000);
 
 
-			panner.setPosition(x/50,y,z/50);
+			panner.setPosition(x/50,y,-z/50);
 			source.playbackRate.value = 1.1-(Math.random()*0.2);
 
 			panner.connect(masterGain);
